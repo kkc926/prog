418b82391d7e70b0b189768015a547d2fc55913c
yongin <kkc926@naver.com>
2021-02-10 20:41:51 +0900

    등굣길

--- programmers/등굣길/ex.py
+++ programmers/등굣길/ex.py
@@ -1,7 +1,10 @@
 def solution(m, n, puddles):
     route = [[0] * (m + 1) for _ in range(n + 1)]
 
+
     route[1][1] = 1
+
+
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             if i == 1 and j == 1:
@@ -10,4 +13,6 @@ def solution(m, n, puddles):
                 route[i][j] = 0
             else:
                 route[i][j] += (route[i - 1][j] + route[i][j - 1])
+
+
     return (route[-1][-1]) % 100000007

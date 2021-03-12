98c4f0e8d519579b63b4365da1726e7188ac2ed7
yongin <kkc926@naver.com>
2021-03-12 22:05:56 +0900

    정렬

--- programmers/삽입정렬/ex.py
+++ programmers/삽입정렬/ex.py
@@ -3,6 +3,7 @@ def insertion_sort(arr):
         temp = arr[i]
         prev = i - 1
         
+        
         while prev >= 0 and arr[prev] > temp:
             arr[prev + 1] = arr[prev]
             prev -= 1

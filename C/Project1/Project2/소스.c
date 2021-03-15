808c3e5fe54c1e0792a77a7fa26c0583dd25084d
yongin <kkc926@naver.com>
2021-03-15 22:00:27 +0900

    c

---
+++ C/Project1/Project2/소스.c
@@ -0,0 +1,23 @@
+#include<stdio.h> //전처리기 printf() 라는 함수를 쓸수 있도록 해줌
+
+void main() { //main함수는 프로그램의 시작을 알려줌
+//void는 매개변수, 형식 등등이 생략된 것이 아닌 없음을 알려줌
+
+	printf("Hello World\n");
+	return 0;
+} //main 함수가 끝나면 프로그램도 끝
+
+
+
+// 컴파일러(compiler) : 소스코드를 (High Level 언어) 기계어로 번역해줌. helloworld.c(설계도) --> helloworld.obj(부품) 파일이 생성됨 
+// 링커(linker) : .obj(부품) 파일을 helloworld.exe(완제품) 파일로 만들어줌 
+
+//프로그램이란 기계적인 행동의 순서를 나타내주는 것
+//{
+//눈을뜬다;
+//불을켠다;
+//화장실에간다;
+//}
+
+
+ 
